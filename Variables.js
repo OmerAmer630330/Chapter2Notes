dcd1daa4a45c6e49f991d8936f4b17a1d5481791
8eel <8eel@fresnou.org>
2023-02-06 13:37:51 -0800

    initial commit

---
+++ Variables.js
@@ -0,0 +1,68 @@
+//Variables store data for later use
+
+//Create a variable with let
+let myVariable ="value";
+let myNewVariable = 7;
+let myBool = true;
+
+//Accessing variable values
+console.log(myVariable);
+console.log(myNewVariable);
+Console.log(myBool);
+
+//Update value stored in variable
+myNewVariable=10;
+console.log(myNewVariable);
+
+//increase by 1
+myNewVariable = myNewVariable-1
+console.log(myNewVariable);
+
+//multiply by 3
+myNewVariable = myNewVariable * 3
+console.log(myNewVariable);
+
+//divide by 3
+myNewVariable = myNewVariable / 3
+console.log (myNewVariable);
+
+//divide and return remainder
+myNewVariable = myNewVariable % 2;
+console.log(myNewVariable);
+
+//shortcuts
+let x = 100;
+
+x = x+1; //add 1
+
+x++; //add 1
+
+x = x - 1; //subtract 1
+
+x--; //subtract 1
+
+x = x + 5; //add 5
+
+x+= 5; //add 5
+
+x = x - 2; //subtract 2
+
+x -= 2; //subtract 2
+
+x = x * 10; //times 10
+
+x *= 10; //times 10
+
+x = x / 10; //divide by 10
+
+x /= 10; //divide by 10
+
+// Keywords
+
+const pi = 3.14;
+const username = "Omer";
+//constants cannot be change
+
+var old = 123;
+//var is the old way to create variables
+//DON'T US IT. use let instead
