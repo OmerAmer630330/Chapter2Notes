daf9bb8e7cbd3555cf2461161c11300e5263e1b1
8eel <8eel@fresnou.org>
2023-02-17 14:14:45 -0800

    updated

---
+++ whileloops1.js
@@ -0,0 +1,37 @@
+const readline = require('readline-sync');
+
+// let x=0;
+// while (x < 26){
+//     console.log(x);
+//     x++
+// }
+// let x=0;
+// while (x < 51){
+//     console.log(x);
+//     x++
+// }
+// let x=-10;
+// while (x < 11){
+//     console.log(x);
+//     x++
+// }
+// let x=0;
+// while (x > -51){
+//     console.log(x);
+//     x--
+// }
+// let x=0;
+// while (x < 101){
+//     console.log(x);
+//     x+=2
+// }
+// let x=100;
+// while (x > -1){
+//     console.log(x);
+//     x-=1
+// }
+// let x=0;
+// while (x < 101){
+//     console.log(x);
+//     x+=3
+// }
