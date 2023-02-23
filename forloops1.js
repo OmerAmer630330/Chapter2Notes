8b2ea8b09c2ea8faf7084c92765f780f34c7cbb7
8eel <8eel@fresnou.org>
2023-02-23 14:18:52 -0800

    forloops1

---
+++ forloops1.js
@@ -0,0 +1,14 @@
+//1
+// for (let i = 0;  i < 151; i++ ){
+//     console.log(i);
+//2
+// for (let i = 0;  i < 40; i++ ){
+//     console.log(i**2);
+//3
+// for (let i = 0;  i < 30; i++ ){
+//     console.log(i**3);
+// }
+for (let i = 0;  i <= 700; i++ ){
+    if (i == 5 )
+    console.log(i+5);
+}
