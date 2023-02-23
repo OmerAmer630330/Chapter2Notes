2faeee2d1f2fdb1b4a8f160c970c872d77341a70
8eel <8eel@fresnou.org>
2023-02-23 13:21:50 -0800

    updated

--- whileloops1.js
+++ whileloops1.js
@@ -1,45 +1,54 @@
 const readline = require('readline-sync');
 
+//1
 // let x=0;
 // while (x < 26){
 //     console.log(x);
 //     x++
 // }
+//2
 // let x=0;
 // while (x < 51){
 //     console.log(x);
 //     x++
 // }
+//3
 // let x=-10;
 // while (x < 11){
 //     console.log(x);
 //     x++
 // }
+//4
 // let x=0;
 // while (x > -51){
 //     console.log(x);
 //     x--
 // }
+//5
 // let x=0;
 // while (x < 101){
 //     console.log(x);
 //     x+=2
 // }
+//6
 // let x=100;
 // while (x > -1){
 //     console.log(x);
 //     x-=1
 // }
+//7
 // let x=0;
 // while (x < 101){
 //     console.log(x);
 //     x+=3
 // }
+//8
 // let x=0;
 // // while (x < 1001){
 // //     console.log(x);
 // //     x+=5
 // // }
+//9
 // let x=0;
 // while (x < 101){
 //     if (x<=9){
@@ -50,14 +59,15 @@ const readline = require('readline-sync');
 //     }
 //     x++
 // }
-let x=0;
-while (x < 101){
-    if (x<=9){
-        console.log(x);
-    } else if (x >=21 && x<=39 ) {
-        console.log(x);
-    } else if (x >=51){
-            console.log(x);
-        }
-    x++
-}
+//10
+// let x=0;
+// while (x < 101){
+//     if (x<=9){
+//         console.log(x);
+//     } else if (x >=21 && x<=39 ) {
+//         console.log(x);
+//     } else if (x >=51){
+//             console.log(x);
+//         }
+//     x++
+// }
