04d2ea3fd065949b6f8bbdaa1f0fe70af47f5742
8eel <8eel@fresnou.org>
2023-02-24 14:18:05 -0800

    updated

--- forloops1.js
+++ forloops1.js
@@ -8,7 +8,45 @@
 // for (let i = 0;  i < 30; i++ ){
 //     console.log(i**3);
 // }
-for (let i = 0;  i <= 700; i++ ){
-    if (i == 5 )
-    console.log(i+5);
-}
+//4
+// for (let i = 0;  i <= 140; i++ ){
+//     console.log(i*5);
+// }
+//5
+// for (let i = 350;  i <= 1000; i++ ){
+//     if (i%10==0){
+//     console.log(i)
+//     }
+// for (let i =1;  i <= 50; i++ ){
+//     if (i%10==0){
+//         console.log(i)
+//     }
+// }
+//6
+// let sum = 0;
+// for (let i = 1; i <= 50; i++ ){
+//     sum += i;
+// }
+// console.log(sum);
+//7
+// let sum = 0;
+// for (let i = 35; i <= 70; i++ ){
+//     sum += i;
+//8
+// let sum=1
+// for (let i = 1; i <= 15; i++){
+//         sum *= i;{
+//     }
+// }
+// console.log(sum);
+//9
+// let sum=10
+// for (let i = 10; i <= 35; i++){
+//     sum *= i;{
+//     }
+// }
+// console.log(sum);
+//10
+// for (let i = 0;  i < 13; i++ ){
+//     console.log("happy birthday");
+// }
