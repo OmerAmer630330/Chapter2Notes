32de98e0b462c82807a02e1c9eb75a6b9f429349
8eel <8eel@fresnou.org>
2023-03-07 14:17:53 -0800

    updated

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+let f = "fizz";
+let b = "buzz";
+for (let num = 1; num <=100 ; num++) {
+    if (num% 3 == f && num % 5 ==b){
+        console.log(num + f + b);
+    }
+    else if (num % 5 == 0){
+        console.log(num+b);
+    }
+    else if (num % 3 == 0){
+        console.log(num+f);
+    }
+    else {
+        console.log(num);
+    }
+}
